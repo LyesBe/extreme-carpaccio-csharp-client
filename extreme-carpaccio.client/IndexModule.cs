5e50745c16978cf6f30501f7b07b03c531b93bca
LyesBe <berkani.lyes@gmail.com>
2016-06-22 11:51:19 +0200

    Commit final usage dictionnaire, tout fonctionne oklm

--- extreme-carpaccio.client/IndexModule.cs
+++ extreme-carpaccio.client/IndexModule.cs
@@ -35,61 +35,40 @@ namespace xCarpaccio.client
                     tot += price;
                 }
 
-                decimal taxes = 0;
-                switch (order.Country)
-                {
-                    case "FI":
-                        taxes = 1.17m;
-                        break;
-                    case "SK":
-                        taxes = 1.18m;
-                        break;
-                    case "ES":
-                    case "CZ":
-                        taxes = 1.19m;
-                        break;
-                    case "DE":
-                    case "FR":
-                    case "RO":
-                    case "NL":
-                    case "EL":
-                    case "LV":
-                    case "MT":
-                        taxes = 1.2m;
-                        break;
-                    case "UK":
-                    case "PL":
-                    case "BG":
-                    case "DK":
-                    case "IE":
-                    case "CY":
-                        taxes = 1.21m;
-                        break;
-                    case "AT":
-                    case "EE":
-                        taxes = 1.22m;
-                        break;
-                    case "LT":
-                    case "HR":
-                    case "SE":
-                    case "PT":
-                        taxes = 1.23m;
-                        break;
-                    case "BE":
-                    case "SI":
-                        taxes = 1.24m;
-                        break;
-                    case "LU":
-                    case "IT":
-                        taxes = 1.25m;
-                        break;
-                    case "HU":
-                        taxes = 1.27m;
-                        break;
-                }
+                Dictionary<string, decimal> taxesdict = new Dictionary<string, decimal>();
 
-                if (taxes != 0)
+                taxesdict.Add("FI", 1.17m);
+                taxesdict.Add("SK", 1.18m);
+                taxesdict.Add("ES", 1.19m);
+                taxesdict.Add("CZ", 1.19m);
+                taxesdict.Add("DE", 1.20m);
+                taxesdict.Add("FR", 1.20m);
+                taxesdict.Add("RO", 1.20m);
+                taxesdict.Add("NL", 1.20m);
+                taxesdict.Add("EL", 1.20m);
+                taxesdict.Add("LV", 1.20m);
+                taxesdict.Add("MT", 1.20m);
+                taxesdict.Add("UK", 1.21m);
+                taxesdict.Add("PL", 1.21m);
+                taxesdict.Add("BG", 1.21m);
+                taxesdict.Add("DK", 1.21m);
+                taxesdict.Add("IE", 1.21m);
+                taxesdict.Add("CY", 1.21m);
+                taxesdict.Add("AT", 1.22m);
+                taxesdict.Add("EE", 1.22m);
+                taxesdict.Add("LT", 1.23m);
+                taxesdict.Add("HR", 1.23m);
+                taxesdict.Add("SE", 1.23m);
+                taxesdict.Add("PT", 1.23m);
+                taxesdict.Add("BE", 1.24m);
+                taxesdict.Add("SI", 1.24m);
+                taxesdict.Add("LU", 1.25m);
+                taxesdict.Add("IT", 1.25m);
+                taxesdict.Add("HU", 1.27m);
+
+                if (taxesdict.ContainsKey(order.Country))
                 {
+                    decimal taxes = taxesdict[order.Country];
 
                     tot = tot * taxes;
 
