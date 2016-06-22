c965ef1d230d3a52c1589d754662318e2af5cac4
unknown <p1006949@IUTDOUAB2PC10.univ-lyon1.fr>
2016-06-22 10:55:27 +0200

    Premier commit

--- extreme-carpaccio.client/IndexModule.cs
+++ extreme-carpaccio.client/IndexModule.cs
@@ -25,8 +25,46 @@ namespace xCarpaccio.client
                 //TODO: do something with order and return a bill if possible
                 // If you manage to get the result, return a Bill object (JSON serialization is done automagically)
                 // Else return a HTTP 404 error : return Negotiate.WithStatusCode(HttpStatusCode.NotFound);
+
+                Console.WriteLine(order.Country);
+                if (order.Country == "DE"){
+                    decimal tot = 0;
+
+                    for (int i = 0; i < order.Prices.Length; i++){
+                        var price = order.Prices[i] * order.Quantities[i];
+                        price = price * 120 / 100;
+                        tot += price;
+                    }
+
+                    if (order.Reduction == "STANDARD")
+                    {
+                        if (tot >= 1000 && tot < 5000)
+                        {
+                            tot = tot * 97 / 100;
+                        }
+                        if (tot >= 5000 && tot < 7000)
+                        {
+                            tot = tot * 95 / 100;
+                        }
+                        if (tot >= 7000 && tot < 10000)
+                        {
+                            tot = tot * 93 / 100;
+                        }
+                        if (tot >= 10000 && tot < 50000)
+                        {
+                            tot = tot * 90 / 100;
+                        }
+                        if (tot >= 50000)
+                        {
+                            tot = tot * 85 / 100;
+                        }
+                    }
+
+                    bill.total = tot;
+
+                }
                 
-                return bill;
+                    return bill;
             };
 
             Post["/feedback"] = _ =>
